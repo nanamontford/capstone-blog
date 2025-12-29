8dd6043de28565283baa39531fef397c61994a39
nanamontford <adwowamontford@gmail.com>
2025-12-29 21:48:29 +0000

    add python version and git ignore

---
+++ a_brave_new_blog_again/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for a_brave_new_blog_again project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_brave_new_blog_again.settings')
+
+application = get_asgi_application()
