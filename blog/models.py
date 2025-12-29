d55f471eb6f468341529d955241265aa8d67579b
nanamontford <adwowamontford@gmail.com>
2025-12-29 22:25:58 +0000

    addd blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+
+    options = (
+        ('drafts', 'Drafts')
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts" )
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    # status = models.IntegerField(choices=STATUS, default=0)
+    # excerpt = models.TextField(blank=True)
+    # updated_on = models.DateTimeField(auto_now=True)
