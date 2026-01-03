969b80f8905435c61bb5bb018b51adb4f4c10084
nanamontford <adwowamontford@gmail.com>
2026-01-03 22:57:33 +0000

    add metadata for admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author')
+
+admin.site.register(models.Post, AuthorAdmin)
+
+
+
+
+
+# from .models import Post
+# admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,20 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 
     options = (
-        ('drafts', 'Drafts')
-        ('published', 'Published')
+        ('drafts', 'Drafts'),
+        ('published', 'Published'),
     )
 
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
+    publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts" )
     content = models.TextField()
     status = models.CharField(max_length=10, choices=options, default='draft')
+    # bookauthor = models.CharField(max_length=250, unique=True)
     # status = models.IntegerField(choices=STATUS, default=0)
     # excerpt = models.TextField(blank=True)
     # updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
